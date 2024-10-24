ba793058d50cf85fa7cc943e0f55ffe115fb41d2
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2024-10-24 15:37:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.6 MB (100.00%)

---
+++ my-folder/0069-sqrtx/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x<0:
+            return("Sq root of negative no are imaginary")
+        else:
+            return(int(x**0.5))
+        
