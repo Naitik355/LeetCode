cd894bcd23fe0f6ececc35d70bd15b424a9213ce
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2024-12-01 07:28:23 +0000

    [LeetCode Sync] Runtime - 15 ms (29.36%), Memory - 12.3 MB (80.26%)

---
+++ my-folder/1468-check-if-n-and-its-double-exist/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        for i in range(len(arr)):
+            for j in (arr[:i]+arr[i+1:]):
+                if 2*arr[i]==j:
+                    return True
+        return False
