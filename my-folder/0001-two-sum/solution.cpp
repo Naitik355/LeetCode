9129eb886129c790c8dfbebda8f5df9c73fcd9de
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2025-07-17 11:20:57 +0000

    [LeetCode Sync] Runtime - 73 ms (9.31%), Memory - 14.1 MB (76.40%)

---
+++ my-folder/0001-two-sum/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>a;
+        for(int i=0;i<nums.size();i++){
+            int sum=0;
+            for(int j=i+1;j<nums.size();j++){
+                sum=nums[i]+nums[j];
+                if(sum==target){
+                    a.push_back(i);
+                    a.push_back(j);
+                    return a;
+                }
+            }
+        }
+        return a;
+    }
+};
