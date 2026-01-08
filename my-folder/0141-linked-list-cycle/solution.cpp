8bdf3958981c738078b53b9e9e7268ffd66042bb
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-01-08 12:57:54 +0000

    [LeetCode Sync] Runtime - 3 ms (99.49%), Memory - 11.8 MB (52.89%)

--- my-folder/0141-linked-list-cycle/solution.cpp
+++ my-folder/0141-linked-list-cycle/solution.cpp
@@ -11,10 +11,10 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
+        while(fast && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-            if(fast==slow) return true;
+            if(slow==fast) return true;
         }
         return false;
     }
