41c9142ad71a5a6e414d838feff9aff339cc3c6f
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-01-08 14:06:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.9 MB (59.13%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.cpp
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -10,26 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count=0;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            count++;
-            temp=temp->next;
-        }
 
-        if (n == count) {
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        while(curr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next; 
         }
-        temp=head;
-        for(int i=1;i<count-n;i++){
-            temp=temp->next;
+        return prev;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head==nullptr) return head;
+        head=reverse(head);
+        ListNode* curr=head;
+        ListNode* prev=nullptr;
+        int count=1;
+        if(n==1){
+            ListNode* temp=head;
+            head=head->next;
+            temp->next=nullptr;
+            delete temp;
+            return reverse(head);
         }
-        ListNode* nodeToDelete = temp->next;
-        temp->next = temp->next->next;
-        delete nodeToDelete;
-        return head;    
+        while(curr){
+            if(count==n){
+                ListNode* temp=curr;
+                prev->next=curr->next;
+                curr=curr->next;
+                temp->next=nullptr;
+                delete temp;
+                break;
+            }
+            prev=curr;
+            curr=curr->next;
+            count++;
+        }
+        return reverse(head);
     }
 };
