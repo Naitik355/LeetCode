59f35a20fc5f216ab5f35976346aeb90ce456ca8
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-01-13 08:38:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (80.21%)

---
+++ my-folder/0342-power-of-four/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        while(n==1) return true;
+        while(n<=0 || n%4!=0) return false;
+        return isPowerOfFour(n/4);
+    }
+};
