1da077396666b16c4196a7c902b55ba7fd2188b1
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-02-15 17:14:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 35.6 MB (96.39%)

---
+++ my-folder/0724-find-pivot-index/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total_sum=0;
+        for(int i:nums) total_sum+=i;
+        int left_sum=0;
+        for(int i=0;i<nums.size();i++){
+            int right_sum=total_sum-left_sum-nums[i];
+
+            if(left_sum==right_sum) return i;
+
+            left_sum+=nums[i];
+        }
+        return -1;
+    }
+};
