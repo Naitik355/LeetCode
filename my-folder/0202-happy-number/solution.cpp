d38746ef448262adbcdd0f244ef7a9994221e158
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-04-01 13:14:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (96.74%)

---
+++ my-folder/0202-happy-number/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum+=d*d;
+            n=n/10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(true){
+        slow=f(slow);
+        fast=f(f(fast));
+        if(slow==fast) break;
+    }
+    return slow==1;
+    }
+};
