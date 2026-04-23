13b1de04675333130368375f24e0411698928417
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-04-23 05:18:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (74.84%)

--- my-folder/0242-valid-anagram/solution.cpp
+++ my-folder/0242-valid-anagram/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return 0;
+        if(s.length()!=t.length()) return false;
         int freq[26]={0};
         for(char c:s) freq[c-'a']++;
         for(char d:t) freq[d-'a']--;
