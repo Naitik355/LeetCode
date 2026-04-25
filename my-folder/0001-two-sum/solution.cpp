dd3acd48dd868f2e7e72678e37fbeca60adf1f1e
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-04-25 05:23:48 +0000

    [LeetCode Sync] Runtime - 3 ms (67.68%), Memory - 14.8 MB (44.85%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             int need=target-nums[i];
             if(mp.find(need)!=mp.end()){
-                return {mp[need],i};
+                return{mp[need],i};
             }
             mp[nums[i]]=i;
         }
