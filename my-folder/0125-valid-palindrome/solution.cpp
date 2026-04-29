aaae662b004edbe219646b2641d00363067c6fbc
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-04-29 05:35:51 +0000

    [LeetCode Sync] Runtime - 4 ms (13.83%), Memory - 9.8 MB (76.79%)

--- my-folder/0125-valid-palindrome/solution.cpp
+++ my-folder/0125-valid-palindrome/solution.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string result="";
-        string revresult="";
-        for(char c:s){
-            if(!isspace(c)&&!ispunct(c)) result+=tolower(c);
-        }
-        revresult=result;
-        reverse(revresult.begin(),revresult.end());
-        cout<< revresult;
-        if(result==revresult){
-            return true;
-        }
-         return false;
-     }
-
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if (!isalnum(s[left])) left++;
+            else if (!isalnum(s[right])) right--;
+            else{
+                if(tolower(s[left])!=tolower(s[right])) return false;
+                left++;
+                right--;
+            }
+        }   
+        return true;
+    }
 };
