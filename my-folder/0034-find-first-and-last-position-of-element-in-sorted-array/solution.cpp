b8d5de19450ed607cde2b28d99ad6ac49580208b
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-08-14 05:44:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (53.25%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int lowerBound(vector<int>& nums,int target){
+    int lb(vector<int>& nums,int target){
         int low=0;
-        int n=nums.size();
-        int high=n-1;
-        int ans=nums.size();
+        int high=nums.size()-1;
+        int ans= nums.size();
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]>=target){
                 ans=mid;
                 high=mid-1;
@@ -17,13 +16,12 @@ public:
         }
         return ans;
     }
-        int upperBound(vector<int>& nums,int target){
+    int ub(vector<int>& nums,int target){
         int low=0;
-        int n=nums.size();
-        int high=n-1;
-        int ans=nums.size();
+        int high=nums.size()-1;
+        int ans= nums.size();
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]>target){
                 ans=mid;
                 high=mid-1;
@@ -34,10 +32,11 @@ public:
         }
         return ans;
     }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb=lowerBound(nums,target);
-        if(lb==nums.size()|| nums[lb]!=target) return {-1,-1};
-        int ub=upperBound(nums,target);
-        return {lb,ub-1};
+        int lower=lb(nums,target);
+        if(lower==nums.size()||nums[lower]!=target) return {-1,-1};
+        int upper=ub(nums,target);
+        return {lower,upper-1};
     }
 };
