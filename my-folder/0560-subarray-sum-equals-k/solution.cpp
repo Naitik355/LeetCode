16cd0eb65c63e01589e34ab4546ab9234eff3525
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-08-17 06:09:42 +0000

    [LeetCode Sync] Runtime - 42 ms (59.03%), Memory - 45.4 MB (70.72%)

---
+++ my-folder/0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count=0;
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int prefix=0 ;   
+        for(int x:nums){
+            prefix+=x;
+            if(mp.find(prefix-k)!=mp.end()) count+=mp[prefix-k];
+            mp[prefix]++;
+        }
+    return count;
+    }
+};
