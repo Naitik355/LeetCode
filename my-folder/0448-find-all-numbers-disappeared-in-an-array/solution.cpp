852d91b83b3537bfbc023ce3a666845cca9d849b
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-09-03 11:24:25 +0000

    [LeetCode Sync] Runtime - 45 ms (19.24%), Memory - 64.1 MB (28.52%)

--- my-folder/0448-find-all-numbers-disappeared-in-an-array/solution.cpp
+++ my-folder/0448-find-all-numbers-disappeared-in-an-array/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        unordered_map<int,int> mp;
-        for(int n:nums){
-            mp[n]++;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
         }
         vector<int>ans;
         for(int i=1;i<=n;i++){
-            if(mp[i]==0) ans.push_back(i);
+            if(mp.find(i)==mp.end()){
+                ans.push_back(i);
+            }
         }
         return ans;
     }
