0d74c8d868c4e242aca373e5f76b430a16f151b6
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-09-24 15:13:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 30.7 MB (98.26%)

---
+++ my-folder/3869-smallest-index-with-digit-sum-equal-to-index/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumDig(int n){
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n=n/10;
+        }
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(sumDig(nums[i])==i) return i;
+        }
+        return -1;
+    }
+};
